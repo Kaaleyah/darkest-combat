569d21bacabb834f9ce9a7e0606ce47b3c9c0d6b
Kaaleyah <furkancanbaytemur@gmail.com>
2020-04-22 21:59:11 +0300

    Created

---
+++ cmake-build-debug/CMakeFiles/darkest_combat.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/darkest_combat.dir/main.cpp.obj"
+  "darkest_combat.exe"
+  "darkest_combat.exe.manifest"
+  "darkest_combat.pdb"
+  "libdarkest_combat.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/darkest_combat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
